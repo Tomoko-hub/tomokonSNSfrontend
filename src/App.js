8b6cacf4e50756703dcf50ffbd2b215929042198
Tomoko-hub <haisaitomoco@gmail.com>
2022-07-25 21:26:11 +0300

    login component #1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 /*import Home from "./pages/home/Home";*/
 import { Home } from "@mui/icons-material";
+import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 
 function App() {
   return (
     /*<Home />*/
-    <Profile />
+    <Login />
   );
 }
 
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./Login.css"
+
+export default function Login() {
+  return (
+    <div className='login'>
+        <div className='loginWrapper'>
+            <div className="loginLeft">
+                <div className="loginLeftTitle">
+                    <h3 className='loginLogo'>Tomokon SNS</h3>
+                    <span className="loginDesc">
+                        Tomokon Dummy SNS
+                    </span>
+                </div>
+            </div>
+            <div className="loginRight">
+                    right
+            </div>
+        </div>
+    </div>
+  )
+}

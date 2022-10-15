b0e9bb081d8c5a5519a50178683fbaa21c627a20
Tomoko-hub <haisaitomoco@gmail.com>
2022-10-15 07:28:22 +0300

    create state file and add AuthActions/Context/Reducer

---
+++ src/state/AuthContexst.jsx
@@ -0,0 +1,15 @@
+//Login Actions
+
+export const LoginStart = (user)=> ({
+    type: "LOGIN_START",
+});
+
+export const LoginSuccess = (user)=> ({
+    type: "LOGIN_SUCCESS",
+    payload: user,
+});
+
+export const LoginError = (error)=> ({
+    type: "LOGIN_ERROR",
+    payload: error
+});
